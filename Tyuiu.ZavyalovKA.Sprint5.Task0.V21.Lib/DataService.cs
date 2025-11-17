4162d8c86b63d1de27e34e7d57ca9de8a48272cb
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-17 12:56:11 +0500

    Task0

--- Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Lib
             double z = (Math.Pow(x, 2) + 1) / Math.Sqrt(4 * Math.Pow(x, 2) - 3);
             z = Math.Round(z, 2);
             File.WriteAllText(path, Convert.ToString(z));
-            return path;
+            return path; 
         }
     }
 }
--- Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Test/DataServiceTest.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task0.V21.Test
     {
         [TestMethod]
         public void TestMethod1()
-        {
+        {   
             string path = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task0.V21";
             FileInfo fileInfo = new FileInfo(path);
             bool FileExist = fileInfo.Exists;

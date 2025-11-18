6fe3798e1816af2fc72f71775bd0c199f6dccf98
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-18 13:40:49 +0500

    Task2

---
+++ Tyuiu.ZavyalovKA.Sprint5.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.ZavyalovKA.Sprint5.Task2.V16.Lib;
+namespace Tyuiu.ZavyalovKA.Sprint5.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task2.V16";
+            FileInfo fileInfo = new FileInfo(path);
+            bool FileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, FileExist);
+        }
+    }
+}
---
+++ Tyuiu.ZavyalovKA.Sprint5.Task2.V16/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using Tyuiu.ZavyalovKA.Sprint5.Task2.V16.Lib;
+int[,] matrix = new int[3, 3] { {2, -4, -8},
+                              {3, -7, -2},
+                              {3, 8, 6} };
+int rows = matrix.GetLength(0);
+int columns = matrix.GetLength(1);
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив: ");
+for (int i =0; i < rows; i++)
+{
+    for(int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"{matrix[i, j]} \t ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string res = ds.SaveToFileTextData(matrix);
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();

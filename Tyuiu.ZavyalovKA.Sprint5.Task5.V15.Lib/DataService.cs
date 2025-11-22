450035c2845995b3ceb6000d52a9afae9e3cd24a
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-22 20:38:45 +0500

    Task 5

--- Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib/DataService.cs
@@ -7,24 +7,25 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double z = double.MaxValue;
-
-            using (StreamReader reader = new StreamReader(path))
+            double min = double.MaxValue;
+            string allText = File.ReadAllText(path);
+            string[] numbers = allText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string numStr in numbers)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string cleanNum = numStr.Trim().Replace(",", ".");
+                if (double.TryParse(cleanNum, out double number))
                 {
-                    if (double.TryParse(line.Trim(), out double number) && number % 5 == 0)
+                    if (number % 5 == 0 && number < min)
                     {
-                        if (number < z)
-                            z = number;
+                        min = number;
                     }
                 }
             }
-            return z == double.MaxValue ? 0 : z;
+            return min == double.MaxValue ? 0 : min;
         }
     }
 }
 
 
-          
+
+

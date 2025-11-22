c9e8a34bda3c8dfc9687b73221107ee46653eb79
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-22 18:04:59 +0500

    Task4

--- Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Lib/DataService.cs
@@ -7,10 +7,11 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double x = Convert.ToDouble(strX);
+            strX = strX.Trim().Replace(" ", "").Replace("\n", "").Replace("\r", "").Replace("\t", "");
+            strX = strX.Replace(",", ".");
+            double x = double.Parse(strX, System.Globalization.CultureInfo.InvariantCulture);
             double res = ((x / 2) * x) + Math.Sin(Math.Pow(x, 2));
-            res = Math.Round(res, 3);
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }

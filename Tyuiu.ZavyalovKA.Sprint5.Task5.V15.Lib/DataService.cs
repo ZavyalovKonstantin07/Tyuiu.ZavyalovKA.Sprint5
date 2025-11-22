6716b1cd70504c2e79381ec91624dbeac8e0bea4
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-22 20:44:54 +0500

    Task 5

--- Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib/DataService.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib/DataService.cs
@@ -8,16 +8,22 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task5.V15.Lib
         public double LoadFromDataFile(string path)
         {
             double min = double.MaxValue;
+
             string allText = File.ReadAllText(path);
-            string[] numbers = allText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] numbers = allText.Split(new char[] { ' ', '\t', '\n', '\r' },
+                                           StringSplitOptions.RemoveEmptyEntries);
+
             foreach (string numStr in numbers)
             {
                 string cleanNum = numStr.Trim().Replace(",", ".");
-                if (double.TryParse(cleanNum, out double number))
+
+                if (double.TryParse(cleanNum, System.Globalization.NumberStyles.Float,
+                                   System.Globalization.CultureInfo.InvariantCulture, out double number))
                 {
-                    if(number % 5 == 0 && number > 0 && number < min)
+                    if (number > 0 && number % 5 == 0)
                     {
-                        min = number;
+                        if (number < min)
+                            min = number;
                     }
                 }
             }

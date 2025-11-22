cdbce52bc44e8ee6aecc790255405920d1604a95
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-22 17:49:12 +0500

    Task4

--- Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Test/DataServiceTest.cs
+++ Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ZavyalovKA.Sprint5.Task4.V29.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task4.V29\bin\Debug\net8.0";
+            string path = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task4.V29";
             FileInfo fileInfo = new FileInfo(path);
             bool FileExist = fileInfo.Exists;
             bool wait = true;

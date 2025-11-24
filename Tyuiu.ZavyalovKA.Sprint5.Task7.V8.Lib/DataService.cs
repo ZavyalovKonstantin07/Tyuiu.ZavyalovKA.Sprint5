6de170df58e0e8e9605691dddca051d46e4e0867
Константин Завьялов <Tyuiu.ZavyalovKA@outlook.com>
2025-11-24 14:53:43 +0500

    Task7

---
+++ Tyuiu.ZavyalovKA.Sprint5.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.Security;
+namespace Tyuiu.ZavyalovKA.Sprint5.Task7.V8.Lib
+{
+    public class DataService : ISprint5Task7V8
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char currentChar = line[i];
+                        if (currentChar >= 'А' && currentChar <= 'Я')
+                        {
+                            strLine = strLine + (char)(currentChar + 32);
+                        }
+                        else
+                        {
+                            strLine = strLine + currentChar;
+                        }
+                    }
+                }
+            }
+            return path;
+        }
+    }
+}
+
---
+++ Tyuiu.ZavyalovKA.Sprint5.Task7.V8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using Tyuiu.ZavyalovKA.Sprint5.Task7.V8.Lib;
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+string path = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task7.V8\bin\Debug\net8.0\InPutDataFileTask7V8.txt";
+string pathSaveFile = @"C:\Users\Завьялов Константин\source\repos\Tyuiu.ZavyalovKA.Sprint5\Tyuiu.ZavyalovKA.Sprint5.Task7.V8\bin\Debug\net8.0\OutPutDataFileTask7V8.txt";
+Console.WriteLine("Данные находятся в файле: " + path);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Находится в файле: ");
+pathSaveFile = ds.LoadDataAndSave(path);
+Console.WriteLine(pathSaveFile);
+Console.ReadKey();
